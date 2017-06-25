1335a153cbeefabfadda30fc69f6bd766b0b3abc
min <maxati9600@gmail.com>
2017-06-25 08:38:08 +0000

    first commit

---
+++ rubyTest/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+	get "welcome/say_hello" => "welcome#say"
+	get "welcome/index" =>"welcome#index"
+	root :to => "welcome#index"
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
